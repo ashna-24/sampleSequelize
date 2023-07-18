8ada036af2553c2a0f9a182840cbed973ae49ea5
Vijay A <vijay.a@zoftsolutions.com>
2023-07-18 18:46:38 +0530

    second commit

--- controllers/sampleController.js
+++ controllers/sampleController.js
@@ -21,46 +21,75 @@ exports.userData = (req, res, next)=>{
         email: Sequelize.STRING
     });
 
+    const Command = sequelize.define('command',{
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        command: Sequelize.STRING
+    });
+
+    Command.belongsTo(User, { foreignKey: 'userId' });
+
     // Sync the model with the database
     sequelize.sync()
     .then(() => {
         console.log('Database and tables created!');
         const usersData = [
             { name: 'John Doe', email: 'johndoe@example.com' },
-            { name: 'Jane Smith', email: 'janesmith@example.com' },
+            { name: 'Albert John', email: 'janesmith@example.com' },
             { name: 'Bob Johnson', email: 'bobjohnson@example.com' }
         ];
+
+        const comdData = [
+            {command: 'Hi,How are you?'},
+            {command: 'Hello, everyone!!'},
+            {command: 'Have a nice day!!'}
+        ]
         // Insert a new user
         User.bulkCreate(usersData)
         .then(userCreate => {
             console.log('Created user:', userCreate.map(user => user.toJSON()));
             res.status(201).json(userCreate);
-            // Query all users
-            User.findAll()
-            .then(users => {
-                console.log('All users:', users.map(user => user.toJSON()));
-                
-                // Update a user
-                User.update({ name: 'Jane Smith' }, { where: {} })
-                .then(numAffected =>{
-                    console.log('Updated user:', numAffected[0]);
-                    
-                    // Delete a user
-                    // User.destroy({ where: {} })
-                    // .then(numAffected => {
-                    //     console.log('Deleted user:', numAffected);
-                    // })
-                    // .catch(err => {
-                    //     console.error('Error deleting user:', err);
-                    // });
-                })
-                .catch(err => {
-                    console.error('Error updating user:', err);
-                });
+
+            Command.bulkCreate(comdData)
+            .then(userCommand =>{
+                console.log('Commands:', userCommand.map(command => command.toJSON()));
             })
-            .catch(err => {
-                console.error('Error querying users:', err);
+
+                
+
+            .catch(err =>{
+                console.log('Error to creating database and table', err);
             });
+
+            // Query all users
+            // User.findAll()
+            // .then(users => {
+            //     console.log('All users:', users.map(user => user.toJSON()));
+                
+            //     // Update a user
+            //     User.update({ name: 'Jane Smith' }, { where: {name: 'John Doe'} })
+            //     .then(() => {
+            //         console.log('Updated user:', users.map(user => user.toJSON()));
+                    
+            //         // Delete a user
+            //         // User.destroy({ where: { id: user.id } })
+            //         // .then(() => {
+            //         //     console.log('Deleted user:', user.toJSON());
+            //         // })
+            //         // .catch(err => {
+            //         //     console.error('Error deleting user:', err);
+            //         // });
+            //     })
+            //     .catch(err => {
+            //         console.error('Error updating user:', err);
+            //     });
+            // })
+            // .catch(err => {
+            //     console.error('Error querying users:', err);
+            // });
         })
         .catch(err => {
             console.error('Error creating user:', err);
